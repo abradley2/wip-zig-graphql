65c60f4e7414d42c81fab9c7f06a75d4cd40d914
Tony Bradley <antbradley91@gmail.com>
2026-01-17 13:24:19 -0500

    common field getter

--- src/ast.zig
+++ src/ast.zig
@@ -42,12 +42,83 @@ pub const Definition: type = union(enum(u8)) {
     union_definition: UnionDefinition,
 };
 
+test "getDefinitionName" {
+    const union_def: Definition = .{
+        .union_definition = UnionDefinition{
+            .name = "MyUnion",
+            .directives = null,
+            .entries = null,
+        },
+    };
+
+    const input_def: Definition = .{
+        .input_definition = InputDefinition{
+            .name = "MyInput",
+            .directives = null,
+            .fields = null,
+        },
+    };
+
+    try std.testing.expectEqualStrings("MyUnion", getDefinitionName(union_def));
+    try std.testing.expectEqualStrings("MyInput", getDefinitionName(input_def));
+}
+
+pub fn getDefinitionName(definition: Definition) []const u8 {
+    return CommonField(Definition, "name").get(definition);
+}
+
+fn CommonField(comptime T: type, comptime field_name: []const u8) type {
+    const type_info = @typeInfo(T);
+    const union_type: std.builtin.Type.Union = switch (type_info) {
+        .@"union" => |v| v,
+        else => @compileError("Must be a union type"),
+    };
+
+    inline for (union_type.fields) |union_field| {
+        const struct_type: std.builtin.Type.Struct = switch (@typeInfo(union_field.type)) {
+            .@"struct" => |v| v,
+            else => @compileError("All members of this union must be a non-pointer struct"),
+        };
+
+        var found: bool = false;
+        inline for (struct_type.fields) |struct_field| {
+            if (std.mem.eql(u8, struct_field.name, field_name)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            @compileError("Type is missing common field");
+        }
+    }
+
+    return struct {
+        const Self = @This();
+        const union_fields = union_type.fields;
+        const UnionEnumType = union_type.tag_type orelse @compileError("Must declare an enum type for this union");
+
+        pub fn get(t: T) []const u8 {
+            const initialized_member_index: usize = @intCast(@intFromEnum(t));
+
+            inline for (Self.union_fields, 0..) |union_field, enum_value| {
+                if (enum_value == initialized_member_index) {
+                    const a: union_field.type = @field(t, union_field.name);
+                    return @field(a, field_name);
+                }
+            }
+            @panic("Union enum index out of bounds");
+        }
+    };
+}
+
 pub const ScalarDefinition: type = struct {
     name: []const u8,
     directives: ?[]Directive,
 };
 
 pub const SchemaDefinition: type = struct {
+    name: []const u8 = "schema",
     directives: ?[]Directive,
     fields: ?[]Field,
 };
